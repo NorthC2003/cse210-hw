01067dbd778bb7f9be6a51a1bd875aecab382c17
NorthC2003 <col21062@byui.edu>
2025-05-01 22:17:21 -0600

    Create ProgramGenerator.cs

---
+++ week02/Journal/ProgramGenerator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    private List<string> _prompts = new List<string>
+    {
+        "What was the best part of your day?",
+        "What are you grateful for today?",
+        "Describe something you learned today.",
+        "How did you show kindness today?",
+        "What goals do you have for tomorrow?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
