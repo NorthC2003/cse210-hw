e896520f7e9051090e8f106882f56a0854331381
NorthC2003 <col21062@byui.edu>
2025-05-22 22:30:57 -0600

    Update Program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the options: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity breath = new BreathingActivity();
+                breath.Run();
+            }
+            else if (choice == "2")
+            {
+                ReflectionActivity reflect = new ReflectionActivity();
+                reflect.Run();
+            }
+            else if (choice == "3")
+            {
+                ListingActivity list = new ListingActivity();
+                list.Run();
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Goodbye Friend!");
+                break;
+            }
+        }
     }
 }
