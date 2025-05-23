e04444f0cd01eb85b8c1c975828d3f772ba79552
NorthC2003 <col21062@byui.edu>
2025-05-22 22:40:25 -0600

    Create MindfulnessActivity.cs

---
+++ week05/Mindfulness/MindfulnessActivity.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Threading;
+
+public class MindfulnessActivity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public MindfulnessActivity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name}.\n");
+        Console.WriteLine(_description + "\n");
+        Console.Write("How long (in seconds) would you like to do this activity? ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(2);
+        Console.WriteLine($"You have completed the {_name} for {_duration} seconds.");
+        ShowSpinner(3);
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+        int i = 0;
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinner[i]);
+            Thread.Sleep(200);
+            Console.Write("\b");
+            i = (i + 1) % spinner.Length;
+        }
+    }
+
+    protected void Countdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+}
